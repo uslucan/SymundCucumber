e9be969182d03445419e39c0b693ca7e6df8d325
uslucan <ahmetuslu2011@gmail.com>
2022-03-02 18:08:13 +0300

    base page updated for Huseyin Hocam's 5 webelement

--- src/test/java/com/symund/pages/BasePage.java
+++ src/test/java/com/symund/pages/BasePage.java
@@ -31,6 +31,25 @@ public abstract class BasePage {
     public WebElement contacts;
 
 
+    //---------------After the framework------------------
+
+    @FindBy (xpath = "//h2[contains(text(),'HK-Test')]")
+    public WebElement fileInfo;
+    @FindBy(css = ".button.new")
+    public WebElement uploadbtn;
+    @FindBy (xpath = "//label[@for='file_upload_start']")
+    public WebElement uploadfile;
+    @FindBy(css = "input.unified-search__form-input")
+    public WebElement searchFile;
+    @FindBy(xpath = "//ul[@*='Files']//a//h3")
+    public WebElement FileSearchResult;
+
+
+    //---------------------------------------------------
+
+
+
+
     public void navigateTo(String module) {
         if (module.equals("talk")) {
             module = "spreed";
